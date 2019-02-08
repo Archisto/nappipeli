ba42593df8fa90d6e2f013dca2abe7284dcb3e11
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2019-02-08 18:48:40 +0200

    Made the app send data to and receive it from Heroku

--- Nappipeli/app/src/main/java/com/laurikosonen/nappipeli/MainActivity.java
+++ Nappipeli/app/src/main/java/com/laurikosonen/nappipeli/MainActivity.java
@@ -5,7 +5,17 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     private String username = "Anonymous";
     private TextView mTextMessage;
     private TextView usernameText;
+    private TextView helloText;
+    private Button helloButton;
+//    private HerokuService service;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -28,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
                     username += "" + counter;
                     usernameText.setText(String.format(getString(R.string.string_username), username));
                     return true;
+                case R.id.navigation_winners:
+                    OnWinnersButtonClick();
+                    return true;
             }
             return false;
         }
@@ -40,9 +56,67 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.text_counter);
         usernameText = (TextView) findViewById(R.id.text_username);
+        helloButton = (Button) findViewById(R.id.button_hello);
+        helloText = (TextView) findViewById(R.id.text_hello);
         mTextMessage.setText(String.format(getString(R.string.string_counter), counter));
         usernameText.setText(String.format(getString(R.string.string_username), username));
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://ratpack-demo-test.herokuapp.com/")
+                .build();
+        final HerokuService service = retrofit.create(HerokuService.class);
+        helloText.setText("Empty");
+
+        helloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<ResponseBody> call = service.hello();
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> c,
+                                           Response<ResponseBody> response) {
+                        try {
+                            helloText.setText(response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            helloText.setText(e.getMessage());
+                        } catch (NullPointerException e) {
+                            e.printStackTrace();
+                            helloText.setText(e.getMessage());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> c, Throwable t) {
+                        t.printStackTrace();
+                        helloText.setText(t.getMessage());
+                    }
+                });
+            }
+        });
+    }
+
+    private void OnWinnersButtonClick() {
+//        Call<ResponseBody> call = service.hello();
+//        call.enqueue(new Callback<ResponseBody>() {
+//            @Override
+//            public void onResponse(Call<ResponseBody> c,
+//                                   Response<ResponseBody> response) {
+//                try {
+//                    helloText.setText(response.body().string());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    helloText.setText(e.getMessage());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<ResponseBody> c, Throwable t) {
+//                t.printStackTrace();
+//                helloText.setText(t.getMessage());
+//            }
+//        });
     }
 }
